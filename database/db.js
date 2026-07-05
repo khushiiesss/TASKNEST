01cd899ecd2344233e0d0c355bd29d7041c92b1c
Khushi <khushiiii526838@gmail.com>
2026-07-06 04:13:00 +0530

    Configure Railway database

--- database/db.js
+++ database/db.js
@@ -1,23 +1,22 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Nahiaapaariapnipe",
-    database: "student_task_manager"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
-
 connection.connect((err) => {
-
     if (err) {
-        console.log("Connection Failed!");
+        console.log("❌ Connection Failed!");
         console.log(err);
         return;
     }
 
     console.log("✅ Connected to MySQL Successfully!");
-
 });
 
 module.exports = connection;
